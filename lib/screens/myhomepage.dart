37c8fa6169192887c69a97575568ed18f9f8ad36
Piotr Jaskulski <piotr.jaskulski@gmail.com>
2020-06-17 10:47:35 +0200

    refactoring - home screen

---
+++ lib/screens/myhomepage.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:toothidentapp/screens/screen22.dart';
+import 'package:toothidentapp/tools/tools.dart';
+
+class MyHomePage extends StatefulWidget {
+  static const String id = 'Home';
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: IconButton(
+          icon: Icon(Icons.info),
+          onPressed: () {
+            showAppAbout(context);
+          },
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 100.0,
+                backgroundImage: AssetImage('images/tooth.jpg'),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 15.0, vertical: 30.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '"Learning to identify isolated teeth is difficult. '
+                      'In order to learn these visual patterns, '
+                      'the beginning osteologist must learn to make a series of '
+                      'observations and judgments about the morphology of the tooth '
+                      'to be identified."',
+                      style: TextStyle(fontSize: 15.0),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      '(White, Black, Folkens, 2011, p. 563)',
+                      style: TextStyle(fontSize: 12.0),
+                      textAlign: TextAlign.right,
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: RaisedButton(
+                        child: Text(
+                          'Start',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        color: Colors.blue,
+                        onPressed: () {
+                          // go to first screen
+                          Navigator.pushNamed(context, Screen22.id);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
